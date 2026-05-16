727304ac704cc63bd378db0da41e9668bc8c0659
vinayak2922k <vinayaksharma2289@gmail.com>
2026-05-16 19:46:25 +0530

    Initial commit: Complete responsive vertical stopwatch app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import { useRef, useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [count,setCount] = useState(0);
+  const timerRef = useRef();
+
+const onStartButton = () => {
+ timerRef.current = setInterval(() => {
+    setCount(count => count + 1)
+ }, 1000);
 }
 
+const onStopButton = () => {
+  clearInterval(timerRef.current);
+}
+
+const onResetButton = () => {
+ clearInterval(timerRef.current);
+ setCount(0);
+}
+
+  return (
+    <>
+    <div>
+      <h1>StopWatch</h1>
+    </div>
+    <div>
+      <p>Your Stop Watch Timer Count:</p>
+    <div>
+      <span>{count}</span>
+    </div>
+      <button onClick={onStartButton}>Start</button>
+      <button onClick={onStopButton}>Stop</button>
+      <button onClick={onResetButton}>Reset</button>
+    </div>
+   
+   </>
+  )
+}
 export default App;
